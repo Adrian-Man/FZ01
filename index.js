b6678265df6d1df5e928eb67156a01ebfeadaed2
Adrian <86820152+Adrian-Man@users.noreply.github.com>
2023-05-02 17:03:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -83,8 +83,8 @@ gui
 let enableModule = [2, 3, 4, 5, 6, 7, 8, 9];
 let dataArray = [];
 var csvLoader = new THREE.FileLoader();
-csvLoader.load('Output.csv', function (data) {
-  console.log("Data", data);
+csvLoader.load('Output.csv', (data) => {
+  console.log('Data', data);
   let rows = data.split('\r\n');
   for (let i = 0; i < enableModule.length; i++) {
     dataArray.push(
